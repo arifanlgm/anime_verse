a8fa868fb809b84a755c0f189c9633e73c480aec
Arifan Lumban Gaol <arifmarbun321@gmail.com>
2025-12-07 02:51:34 +0700

    build release - build apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,24 @@
+// top-level imports required in Kotlin DSL
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// Load keystore properties
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    FileInputStream(keystorePropertiesFile).use {
+        keystoreProperties.load(it)
+    }
 }
 
 android {
@@ -20,6 +35,22 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as? String
+            keyPassword = keystoreProperties["keyPassword"] as? String
+
+            // ✅ KOREKSI FINAL: Gunakan path relatif naik satu level (dari 'app' ke 'android')
+            // rootProject.file("key.properties") berada di folder 'android'. 
+            // Kita harus mencari file .jks relatif dari lokasi itu.
+            // Gunakan path relatif dari build.gradle level app (yang ada di 'android/app/').
+            // '..': Naik satu level ke folder 'android/'
+            storeFile = file("../${keystoreProperties["storeFile"] as? String}")
+
+            storePassword = keystoreProperties["storePassword"] as? String
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.anime_verse"
@@ -34,8 +65,8 @@ android {
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Signing with the debug keys for now, so flutter run --release works.
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
